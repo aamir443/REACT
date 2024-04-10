997f27f9c0f38c70f0656adb8463d2fe0f8a1d26
aamir930 <mohammadaamir2002@gmail.com>
2024-04-10 15:35:24 +0530

    Day7

---
+++ second-app/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Message  from './Component/Message'
+import Languages from './Component/Languages'
+
+
+function App() {
+
+  return (
+    <>
+     <h1>hello satyam</h1>
+     <Message/>
+     <Languages/>
+    </>
+  )
+}
+
+export default App
+
---
+++ second-app/src/Component/Languages.jsx
@@ -0,0 +1,8 @@
+
+
+const Languages= () => {
+  return (
+    <div>message2</div>
+  )
+}
+export default Languages
